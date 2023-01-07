32c42fc5dfca8467a678f1b526733700b5bcbfb6
David <djmiller1008@yahoo.com>
2023-01-07 14:04:39 +0800

    Remove usecreateindex from models

--- server/models/Abode.js
+++ server/models/Abode.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-mongoose.set("useCreateIndex", true);
 const Schema = mongoose.Schema;
 
 const AbodeSchema = new Schema({
--- server/models/Emblem.js
+++ server/models/Emblem.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-mongoose.set("useCreateIndex", true);
 const Schema = mongoose.Schema;
 
 const EmblemSchema = new Schema({
