e93275d186cc84886ea4b07ed8ff3e3656f66a94
David <djmiller1008@yahoo.com>
2023-01-07 14:46:27 +0800

    Add remaining fields to god type

--- server/schema/god_type.js
+++ server/schema/god_type.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 const graphql = require("graphql");
+const AbodeType = require("./abode_type");
+const EmblemType = require("./emblem_type");
+const Abode = mongoose.model('abode');
+const God = mongoose.model('god');
 const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLList } = graphql;
 
 const GodType = new GraphQLObjectType({
@@ -9,7 +13,41 @@ const GodType = new GraphQLObjectType({
       name: { type: GraphQLString },
       type: { type: GraphQLString },
       description: { type: GraphQLString },
-      domains: { type: new GraphQLList(GraphQLString) }
+      domains: { type: new GraphQLList(GraphQLString) },
+      abode: {
+        type: AbodeType,
+        resolve(parentValue) {
+            return Abode.findById(parentValue.abode)
+                .then(abode => abode)
+                .catch(err => null)
+        }
+      },
+      emblems: {
+        type: new GraphQLList(EmblemType),
+        resolve(parentValue) {
+            return God.findById(parentValue.id)
+                .populate("emblems")
+                .then(god => god.emblems)
+        }
+      },
+      parents: {
+        type: new GraphQLList(GodType),
+        resolve(parentValue) {
+            return God.findRelatives(parentValue.id, 'parents');
+        }
+      },
+      children: {
+        type: new GraphQLList(GodType),
+        resolve(parentValue) {
+            return God.findRelatives(parentValue.id, 'children');
+        }
+      },
+      siblings: {
+        type: new GraphQLList(GodType),
+        resolve(parentValue) {
+            return God.findRelatives(parentValue.id, 'siblings');
+        }
+      }
     })
   });
   
