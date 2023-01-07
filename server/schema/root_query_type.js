013c1096455b3c57bcb3ed11bd27efd6a3a7976e
David <djmiller1008@yahoo.com>
2023-01-07 14:29:09 +0800

    Add abodes and emblems to the root query

--- server/schema/root_query_type.js
+++ server/schema/root_query_type.js
@@ -5,6 +5,11 @@ const { GraphQLObjectType, GraphQLList, GraphQLID, GraphQLNonNull } = graphql;
 const GodType = require("./god_type");
 const God = mongoose.model("god");
 
+const EmblemType = require("./emblem_type");
+const AbodeType = require("./abode_type");
+const Abode = require("../models/Abode");
+const Emblem = mongoose.model("emblem");
+
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: () => ({
@@ -20,6 +25,32 @@ const RootQuery = new GraphQLObjectType({
       resolve(parentValue, { id }) {
         return God.findById(id);
       }
+    },
+    emblems: {
+        type: new GraphQLList(EmblemType),
+        resolve() {
+            return Emblem.find({});
+        }
+    },
+    emblem: {
+        type: EmblemType,
+        args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+        resolve(parentValue, { id }) {
+            return Emblem.findById(id);
+        }
+    },
+    abodes: {
+        type: new GraphQLList(AbodeType),
+        resolve() {
+            return Abode.find({});
+        }
+    },
+    abode: {
+        type: AbodeType,
+        args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+        resolve(parentValue, { id }) {
+            return Abode.findById(id);
+        }
     }
   })
 });
