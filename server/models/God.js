9658c9eaff32ac9a5bebbdbb23471b21e4569306
David <djmiller1008@yahoo.com>
2023-01-10 15:34:47 +0800

    Add static methods, removeRelative and addEmblem

--- server/models/God.js
+++ server/models/God.js
@@ -56,9 +56,9 @@ GodSchema.statics.findRelatives = function(godId, type) {
         .then(god => god[type])
 }
 
-GodSchema.statics.addRelative = async function(godId, relativeId, relationship) {
+GodSchema.statics.addRelative = function(godId, relativeId, relationship) {
   const God = mongoose.model("god");
-  return await God.find({
+  return God.find({
     '_id': { $in: [
         godId,
         relativeId
@@ -87,6 +87,67 @@ GodSchema.statics.addRelative = async function(godId, relativeId, relationship) 
           ([god, relative]) => god
         );
     })
+}
+
+GodSchema.statics.removeRelative = function(godId, relativeId, relationship) {
+  const God = mongoose.model("god");
+  return God.find({
+    '_id': { $in: [
+        godId,
+        relativeId
+    ]}})
+    .then(gods => {
+        const god = godId === gods[0].id ? gods[0] : gods[1];
+        const relative = relativeId === gods[0].id ? gods[0] : gods[1];
+
+        switch (relationship) {
+          case 'parent':
+            god.parents.pull(relative);
+            relative.children.pull(god);
+            break;
+          case 'child':
+            god.children.pull(relative);
+            relative.parents.pull(god);
+            break;
+          case 'sibling':
+            god.siblings.pull(relative);
+            relative.siblings.pull(god);
+            break;
+        }
+    
+       
+        return Promise.all([god.save(), relative.save()]).then(
+          ([god, relative]) => god
+        );
+    })
+}
+
+GodSchema.statics.addEmblem = function(godId, emblemId) {
+  const God = mongoose.model("god");
+  const Emblem = mongoose.model("emblem");
+  let alreadyHasEmblem = false;
+
+
+  return God.findById(godId).then(god => {
+    return Emblem.findById(emblemId).then(emblem => {
+      god.emblems.forEach(em => {
+        if (em._id.equals(emblem._id)) {
+          alreadyHasEmblem = true;
+        }
+      })
+
+     
+      if (!alreadyHasEmblem) {
+        god.emblems.push(emblem);
+        emblem.gods.push(god);
+        return Promise.all([god.save(), emblem.save()]).then(
+          ([god, emblem]) => god
+        )
+      }
+      
+    })
+  });
+
 }
 
 module.exports = mongoose.model("god", GodSchema);
