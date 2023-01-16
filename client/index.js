56b9ca6a940e52abe7fe5aa660abdd92fc3abc8a
David <djmiller1008@yahoo.com>
2023-01-16 20:38:24 +0800

    Configure apollo client, wrap app in apollo provider and hashrouter

--- client/index.js
+++ client/index.js
@@ -1,8 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import App from "./components/App";
+import { ApolloProvider } from "react-apollo";
+import { HashRouter } from "react-router-dom";
+
+const cache = new InMemoryCache({
+    dataIdFromObject: object => object.id || null
+});
+
+const client = new ApolloClient({
+    uri: "http://localhost:5000/graphql",
+    cache: cache,
+    onError: ({ networkError, graphQLErrors }) => {
+        console.log("graphQLErrors", graphQLErrors);
+        console.log("networkError", networkError);
+    }
+});
+
 
 const Root = () => {
-  return <div>Hello, world!</div>;
+  return (
+    <ApolloProvider client={client}>
+        <HashRouter>
+            <App />
+        </HashRouter>
+    </ApolloProvider>
+  )
 };
 
 ReactDOM.render(<Root />, document.querySelector("#root"));
