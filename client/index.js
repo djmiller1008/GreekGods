53889eedc989117f9571171d3c022705c986da0c
David <djmiller1008@yahoo.com>
2023-01-16 20:19:36 +0800

    Add basic html and index file for react entry point

---
+++ client/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Root = () => {
+  return <div>Hello, world!</div>;
+};
+
+ReactDOM.render(<Root />, document.querySelector("#root"));
