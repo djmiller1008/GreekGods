9fe33d8c4edb292ec10b98a2b280a8273550cfcd
David <djmiller1008@yahoo.com>
2023-01-16 23:51:45 +0800

    Render route for godcreate component

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import GodCreate from "./create/GodCreatev2";
 import GodsList from "./gods/GodsList";
 
 const App = () => {
     return (
         <div>
+            <Route exact path="/create" component={GodCreate} />
             <Route exact path="/" component={GodsList} />
         </div>
     )
