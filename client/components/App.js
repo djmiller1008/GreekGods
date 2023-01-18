9fdbf16947d02a0faee715b49f8a6fef3e73ad97
David <djmiller1008@yahoo.com>
2023-01-18 15:36:33 +0800

    Remove godcreate component and add createindex component

--- client/components/App.js
+++ client/components/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route } from "react-router-dom";
-import GodCreate from "./create/GodCreatev2";
+import GodCreate from "./create/GodCreate";
 import GodsList from "./gods/GodsList";
+import { CreateIndex } from "./create/CreateIndex";
+import { Nav } from "./header/Nav";
 
 const App = () => {
     return (
         <div>
-            <Route exact path="/create" component={GodCreate} />
+            <Nav />
+            <Route exact path="/new" component={CreateIndex} />
             <Route exact path="/" component={GodsList} />
         </div>
     )
