1e86fc9927f9658eaa7672f2bba53760f6217fe5
David <djmiller1008@yahoo.com>
2023-01-18 14:21:54 +0800

    Add newEmblem and newAbode mutations for frontend

--- client/graphql/mutations.js
+++ client/graphql/mutations.js
@@ -17,5 +17,25 @@ export default {
                 description 
             }
         }
+    `,
+
+    NEW_EMBLEM: gql`
+        mutation NewEmblem($name: String) {
+            newEmblem(name: $name) {
+                id,
+                name
+            }
+        }
+        
+    `,
+
+    NEW_ABODE: gql`
+        mutation NewAbode($name: String, $coordinates: String) {
+            newAbode(name: $name, coordinates: $coordinates) {
+                id,
+                name,
+                coordinates
+            }
+        }
     `
 }
