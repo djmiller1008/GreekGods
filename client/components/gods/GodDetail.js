0960a5db33b17ccfc14a49456c7d0a01859a5343
David <djmiller1008@yahoo.com>
2023-01-19 18:00:54 +0800

    Render DomainDetail and AbodeDetail components

--- client/components/gods/GodDetail.js
+++ client/components/gods/GodDetail.js
@@ -4,6 +4,8 @@ import NameDetail from "./detail/NameDetail";
 import Queries from "../../graphql/queries";
 import TypeDetail from "./detail/TypeDetail";
 import DescriptionDetail from "./detail/DescriptionDetail";
+import DomainDetail from "./detail/DomainDetail";
+import AbodeDetail from "./detail/AbodeDetail";
 const { FETCH_GOD } = Queries;
 
 const GodDetail = props => {
@@ -12,12 +14,13 @@ const GodDetail = props => {
             {({ loading, error, data }) => {
                 if (loading) return <p>Loading...</p>;
                 if (error) return <p>Error</p>;
-
                 return (
                     <div className="detail">
                         <NameDetail id={data.god.id} name={data.god.name} />
                         <TypeDetail id={data.god.id} type={data.god.type} />
                         <DescriptionDetail id={data.god.id} description={data.god.description} />
+                        <DomainDetail id={data.god.id} domains={data.god.domains} />
+                        <AbodeDetail id={data.god.id} abode={data.god.abode} />
                     </div>
                 );
             }}
